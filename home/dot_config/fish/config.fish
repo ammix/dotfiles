bb9f984a096aa34db1a982ab5c9afedadf270a6c
ammix <maxim@ammix.dev>
2026-08-09 20:51:26 +0200

    add asdf config to fish

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -98,3 +98,17 @@ status is-interactive; and begin
         source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
     end
 end
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
