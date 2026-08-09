eaac829e4c255d31d630b9214547fc9362602091
ammix <maxim@ammix.dev>
2026-08-10 01:20:56 +0200

    rust: add rustup

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -88,6 +88,9 @@ status is-interactive; and begin
         starship init fish | source
     end
 
+    # cargo
+    source "$HOME/.cargo/env.fish"
+
     if set -q KITTY_INSTALLATION_DIR
         set --global KITTY_SHELL_INTEGRATION no-rc
         source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
