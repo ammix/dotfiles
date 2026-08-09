30ba49b2d8a1119e7c6eeae6ddc22c33f3145d8e
ammix <maxim@ammix.dev>
2026-08-09 21:29:23 +0200

    remove some stale shell configs

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -27,7 +27,7 @@ set -gx OPENCODE_EXPERIMENTAL true
 set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
 set -gx SSH_AUTH_SOCK "$HOME/.1password/agent.sock"
 set -gx MAKEFLAGS "--jobs="(nproc)
-fish_add_path "$HOME/.local/bin" "$HOME/.bun/bin" "$HOME/.cargo/bin" /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+fish_add_path "$HOME/.local/bin" "$HOME/.cargo/bin"
 
 status is-interactive; and begin
     # Keep the logical /home path so Starship recognizes home on Atomic systems.
