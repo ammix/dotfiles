12523d5797ff9866dc94fd9a87d89519ff79ed36
ammix <maxim@ammix.dev>
2026-08-17 21:38:16 +0200

    add neogit and make meld mergetool

--- home/dot_config/nvim/plugin/55_plugins.lua
+++ home/dot_config/nvim/plugin/55_plugins.lua
@@ -42,6 +42,11 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 -- Typst
 require("typst-preview").setup({})
 
+-- Neogit
+require("neogit").setup({
+  integrations = { diffview = true },
+})
+
 -- Leap
 require("leap").opts.preview = function(ch0, ch1, ch2)
   return not (ch1:match("%s") or (ch0:match("%a") and ch1:match("%a") and ch2:match("%a")))
