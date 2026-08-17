8efc6cb8db3af3335c9d46f18431aa76dc4c71bd
ammix <maxim@ammix.dev>
2026-08-17 17:53:44 +0200

    nvim: simplify chezmoi integration

--- home/dot_config/nvim/plugin/50_chezmoi.lua
+++ home/dot_config/nvim/plugin/50_chezmoi.lua
@@ -1,25 +1,20 @@
-local chezmoi = require("chezmoi")
+local source_dir = vim.fn.expand("~/.local/share/chezmoi")
 
-local source_result = vim.system({ "chezmoi", "source-path" }, { text = true }):wait()
-if source_result.code ~= 0 then
-  error(source_result.stderr)
-end
-local source_dir = assert(vim.uv.fs_realpath(vim.trim(source_result.stdout)))
-
-chezmoi.setup({
-  extra_args = { "--source", source_dir },
+require("chezmoi").setup({
   edit = {
     watch = true,
   },
+  extra_args = { "--source", source_dir },
 })
 
 local chezmoi_edit = require("chezmoi.commands.__edit")
 local chezmoi_pick = require("chezmoi.pick")
+local pick_args = { "--path-style", "absolute", "--include", "files", "--exclude", "externals" }
 local watch_group = vim.api.nvim_create_augroup("chezmoi_edit_watch", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   group = watch_group,
-  pattern = vim.fs.joinpath(source_dir, "*"),
+  pattern = vim.fs.joinpath(source_dir, "home", "*"),
   callback = function(event)
     vim.schedule(function()
       chezmoi_edit.watch(event.buf)
@@ -28,12 +23,5 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 })
 
 vim.keymap.set("n", "<leader>fc", function()
-  chezmoi_pick.mini(nil, {
-    "--path-style",
-    "absolute",
-    "--include",
-    "files",
-    "--exclude",
-    "externals",
-  })
+  chezmoi_pick.mini(nil, pick_args)
 end, { desc = "Find chezmoi files" })
