1a51cd18179f6b15883eb0bb58a115207bd0f38d
ammix <maxim@ammix.dev>
2026-08-25 15:59:40 +0200

    nvim: preserve filetypes for chezmoi templates

--- home/dot_config/nvim/plugin/15_filetypes.lua
+++ home/dot_config/nvim/plugin/15_filetypes.lua
@@ -1,12 +1,26 @@
+local function match_template(path, bufnr)
+  local filename = path:gsub("%.tmpl$", "")
+  local args = { filename = filename }
+
+  if bufnr >= 0 then
+    args.buf = bufnr
+  end
+
+  return vim.filetype.match(args)
+end
+
 vim.filetype.add({
   filename = {
     ["vifmrc"] = "vim",
   },
   pattern = {
+    [".*/bat/config%.tmpl"] = { "conf", { priority = 10 } },
     [".*/ghostty/config"] = "ghostty",
     [".*/ghostty/config.ghostty"] = "ghostty",
+    [".*/git/config%.tmpl"] = { "gitconfig", { priority = 10 } },
     [".*/hypr/.+%.conf"] = "hyprlang",
     ["%.env%.[%w_.-]+"] = "sh",
+    [".+%.tmpl"] = match_template,
   },
 })
 
