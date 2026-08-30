d0b12c4bdb91d1b22b149d0d9ebd3034ae0d6207
ammix <maxim@ammix.dev>
2026-08-30 22:57:36 +0200

    distrobox: add packaging environment

---
+++ scripts/setup-distrobox.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+BOX="packaging"
+
+distrobox create \
+	--name "$BOX" \
+	--image quay.io/fedora/fedora:rawhide \
+	--additional-packages "sudo-rs fish rpm-build rpmdevtools"
+
+distrobox enter "$BOX" -- bash -c "
+  sudo dnf install -y \
+    --nogpgcheck \
+    --repofrompath 'terra,https://repos.fyralabs.com/terra\$releasever' \
+    terra-release
+
+  sudo dnf install -y \
+    cargo2rpm \
+    go2rpm \
+    anda \
+    zoxide \
+    fzf \
+    starship \
+    jujutsu \
+    terra-mock-configs \
+    fedpkg \
+    rpmlint \
+    lsd \
+    bat \
+    ripgrep \
+    fd-find
+"
