936137c63d9a6c3c205fcc6a48319ff5dabda3e1
ammix <maxim@ammix.dev>
2026-09-08 20:47:28 +0200

    init: switch from cosmic to gnome flatpaks

--- scripts/setup-flatpaks.sh
+++ scripts/setup-flatpaks.sh
@@ -15,20 +15,20 @@ apps=(
 	com.github.jeromerobert.pdfarranger
 	com.github.tchx84.Flatseal
 	com.heroicgameslauncher.hgl
+	com.mattjakeman.ExtensionManager
 	com.tutanota.Tutanota
 	com.valvesoftware.Steam
 	com.vysp3r.ProtonPlus
-	dev.edfloreshz.CosmicTweaks
 	info.febvre.Komikku
 	io.bassi.Amberol
 	io.github.Faugus.faugus-launcher
 	io.github.flattool.Warehouse
 	io.github.idevecore.Valuta
-	io.github.seadve.Kooha
 	io.gitlab.librewolf-community
 	md.obsidian.Obsidian
 	org.gnome.Loupe
 	org.gnome.Papers
+	org.gnome.font-viewer
 	org.gnome.gitlab.YaLTeR.Identity
 	org.gnome.gitlab.YaLTeR.VideoTrimmer
 	org.gtk.Gtk3theme.adw-gtk3
@@ -37,6 +37,7 @@ apps=(
 	org.kde.drawy
 	org.localsend.localsend_app
 	org.qbittorrent.qBittorrent
+	page.tesk.Refine
 	fr.handbrake.ghb
 )
 
